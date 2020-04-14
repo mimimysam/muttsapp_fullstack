b0cde8835c9d0ec698193eb38c9a0272a9cd9b97
mimimysam <59676205+mimimysam@users.noreply.github.com>
2020-04-14 16:50:22 -0700

    first push

---
+++ src/main/java/com/javasampleapproach/springsecurity/jdbcauthentication/MySQL_MVC.java
@@ -0,0 +1,13 @@
+package com.javasampleapproach.springsecurity.jdbcauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySQL_MVC {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MySQL_MVC.class, args);
+	}
+}
---
+++ src/main/java/com/javasampleapproach/springsecurity/jdbcauthentication/controller/WebController.java
@@ -0,0 +1,48 @@
+package com.javasampleapproach.springsecurity.jdbcauthentication.controller;
+
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+
+@Controller
+public class WebController {
+
+   
+    @RequestMapping(value="/")
+    public String home(){
+
+        return "home";
+    }
+   
+    @RequestMapping(value="/user")
+    public String user(Authentication authentication){
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        System.out.println("User has authorities: " + userDetails.getAuthorities());
+
+        return "user";
+    }
+  
+    @RequestMapping(value="/admin")
+    public String admin(Model model){
+
+        return "admin";
+
+    }
+   
+    @RequestMapping(value="/login")
+    public String login(){
+
+        return "login";
+    }
+   
+    @RequestMapping(value="/403")
+    public String Error403(){
+
+        return "403";
+    }
+}
