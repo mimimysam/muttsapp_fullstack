86d8ba4a6f63ac4a3e220edb926ab55e81ad9f4a
mimimysam <59676205+mimimysam@users.noreply.github.com>
2020-07-14 21:41:11 -0700

    updated db connection to AWS RDS instance and prepared pom.xml to package as .war file

--- src/main/java/com/muttsapp/MuttsApp.java
+++ src/main/java/com/muttsapp/MuttsApp.java
@@ -2,19 +2,21 @@ package com.muttsapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.context.annotation.Bean;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
-//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-//import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
 
 @SpringBootApplication
-//@EnableJpaRepositories("muttsapp.repositories")
 @ComponentScan("com.muttsapp")
-public class MuttsApp {
+public class MuttsApp extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(MuttsApp.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MuttsApp.class);
 	}
 
 }
